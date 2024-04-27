502b542b40f52a70532cbc551de0fb9a5e4788c2
ElizabethBenitez20 <141886983+ElizabethBenitez20@users.noreply.github.com>
2024-04-26 21:00:03 -0400

    Added footer to project

--- src/index.js
+++ src/index.js
@@ -9,6 +9,25 @@ root.render(
   <React.StrictMode>
     <h1>Weather App</h1>
     <App />
+    <hr />
+    <h3>
+      This code was coded by{" "}
+      <a href="https://github.com/ElizabethBenitez20" target="_blank">
+        Elizabeth Benitez
+      </a>
+      , is{" "}
+      <a
+        href="https://github.com/ElizabethBenitez20/weather-app-react"
+        target="_blank"
+      >
+        open source
+      </a>
+      , and is{" "}
+      <a href="https://eb-weather-app-react.netlify.app/" target="_blank">
+        hosted on Netlify
+      </a>
+      .
+    </h3>
   </React.StrictMode>
 );
 
